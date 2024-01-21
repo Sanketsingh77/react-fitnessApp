197fe0667af4044bf5f3fb817aef7042b50be9bd
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2024-01-21 18:16:54 +0530

    requests

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+
 const App = () => {
   return (
     <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
